408bb0932f0db26b4642db18dd5a09249af92557
riteshmukim <riteshmukim@gmail.com>
2018-07-15 13:58:30 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print('Hello World')
+
+print('What is your name?')
+name = input()
+print('''It's good to meet you,''',name)
+
+print('The length of your name is :',len(name))
+
+print('What is your age?')
+age = input()
+print('You will be '+str(int(age)+1)+' in a year.')
+
